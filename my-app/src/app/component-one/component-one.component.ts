be32e668179d1a21d4042bd564954c4c7d85c894
Waldemir Junior <waldemir.junior@telefonica.com>
2020-09-01 22:26:10 -0300

    o componente componenteOne virou uma pokedex

--- my-app/src/app/component-one/component-one.component.ts
+++ my-app/src/app/component-one/component-one.component.ts
@@ -5,11 +5,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './component-one.component.html',
   styleUrls: ['./component-one.component.scss']
 })
+
 export class ComponentOneComponent implements OnInit {
+
+  private pokemons: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.fetchPokedex();
   }
 
+  private fetchPokedex(): void {
+    console.log('fetching...');
+
+    fetch('https://pokeapi.co/api/v2/pokedex/2/')
+      .then(response => response.json())
+      .then(({ pokemon_entries }) => {
+        pokemon_entries.forEach(({ entry_number: id, pokemon_species: { name,  url}}) => {
+          this.pokemons.push({
+            id,
+            url,
+            name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+          });
+        });
+      })
+      .catch(error => {
+        console.error('Ops, algo deu errado...');
+        console.error(error);
+
+        alert('Ops, algo deu errado...');
+      });
+  }
 }
