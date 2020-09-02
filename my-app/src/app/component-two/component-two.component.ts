6e4c52d3b6cf4c85a584dace4a56500c72b6d2fb
Waldemir Junior <waldemir.junior@telefonica.com>
2020-09-01 23:03:22 -0300

    atualiza o componente componentTwo

--- my-app/src/app/component-two/component-two.component.ts
+++ my-app/src/app/component-two/component-two.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-component-two',
   templateUrl: './component-two.component.html',
   styleUrls: ['./component-two.component.scss']
 })
+
 export class ComponentTwoComponent implements OnInit {
+
+  @Input() id: string;
+  @Input() url: string;
+  @Input() name: string;
+  @Input() image: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
